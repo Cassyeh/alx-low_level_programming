56af891c9cad6fee4145dc261b33e2c5b7ba915b
Cassyeh <cassandraijezie@gmail.com>
2022-12-09 20:32:35 -0800

    Print triple digit combinations Task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	int n1;
+
+	int n2;
+
+	for (n2 = '0'; n2 <= '9'; n2++)
+	{
+		for (n1= (n2 + 1); n1<= '9'; n1++)
+		{
+			for (n= (n1+ 1); n<= '9'; n++)
+			{
+				putchar(n2);
+				putchar(n1);
+				putchar(n);
+				if (n2 != '7' || n1!= '8' || n!= '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
