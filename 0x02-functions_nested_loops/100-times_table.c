bd290d268b9edcb719877317ca7f117108c52fa4
Cassyeh <cassandraijezie@gmail.com>
2022-12-13 20:52:04 -0800

    Print multiples of 3 Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,5 @@
 #include "main.h"
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
  */
 void print_times_table(int n)
 {
@@ -47,5 +45,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
-
