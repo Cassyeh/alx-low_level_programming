69f21411cce07101f2209fbeb40519086b38254f
Cassyeh <cassandraijezie@gmail.com>
2022-12-13 17:38:09 -0800

    Print _putchar Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,14 @@
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
-	int i, s;
-
-	s = sizeof(str) / sizeof(int);
-	for (i = 0; i < s; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
