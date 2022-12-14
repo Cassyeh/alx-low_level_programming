3e38c13750bd6295873c8617d38bb6408950d6f8
Cassyeh <cassandraijezie@gmail.com>
2022-12-13 17:58:24 -0800

    Print alphabet ten times Task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 
 	char alphabet;
 
-	for(x = 1; x <= 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	_putchar(alphabet);
 	}
