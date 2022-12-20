c96219056728e4f0a9a9db68dbd38f590dd078a4
Cassyeh <cassandraijezie@gmail.com>
2022-12-19 16:03:40 -0800

    Print every other character of a string Task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int length = 0;
 
 	while (str[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0' && i <= length - 1)
 	{
 		_putchar(str[i]);
 		i = i + 2;
