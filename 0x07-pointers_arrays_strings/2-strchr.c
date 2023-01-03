eaa5c594c7e16bb35b8fedc42352eec7aeb31692
Cassyeh <cassandraijezie@gmail.com>
2023-01-02 17:36:01 -0800

    First occurrence of character in string Task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to be checked
+ * @c: character to locate
+ *
+ * Return: pointer to s[firt occurence of c] or
+ * NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
