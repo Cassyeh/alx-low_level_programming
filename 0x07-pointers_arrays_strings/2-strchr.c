652a6f238bc871236fb9441e09d173bf18446298
Cassyeh <cassandraijezie@gmail.com>
2023-01-02 17:41:24 -0800

    Updated find character in string Task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL 0
 
 /**
  * *_strchr - locates a character in a string
@@ -20,5 +19,5 @@ char *_strchr(char *s, char c)
 			return (&s[i]);
 		}
 	}
-	return (NULL);
+	return (0);
 }
