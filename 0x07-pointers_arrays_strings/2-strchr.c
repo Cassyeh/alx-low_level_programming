e45723b3320a1932fabd3729a3297850787159ee
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-01-03 02:48:40 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,8 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	int length = 0;
 	
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	for (i = 0; i < length; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
