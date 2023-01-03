0bc908c0e1b3543bd981993d34dac7c4828b795a
Cassyeh <cassandraijezie@gmail.com>
2023-01-02 19:14:09 -0800

    Locate substring Task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to be searched
+ * @needle: string to be used to search
+ *
+ * Return: pointer to beginning of substring in haystack
+ * or NULL if no match found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int length = 0;
+	int i = 0;
+	int a;
+	int b;
+	int c;
+
+	while (haystack[length] != '\0')
+	{
+		length++;
+	}
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+	for (a = 0; a < length; a++)
+	{
+		c = a;
+
+		for (b = 0; b < i; b++)
+		{
+			if (haystack[c] == needle[b])
+			{
+				c++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (needle[b] == '\0' && haystack[a] == needle[0])
+		{
+			return (&haystack[a]);
+		}
+	}
+	return (0);
+}
