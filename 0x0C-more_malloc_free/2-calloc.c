631b9870c59136274baaae77e0b831ed8fa93688
Cassyeh <cassandraijezie@gmail.com>
2023-01-12 17:53:02 -0800

    Calloc function for array of nmemb elements and size bytes Task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,8 +10,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
-	unsigned int i;
+	void *array;
 
 	if ((nmemb == 0) | (size == 0))
 	{
@@ -21,10 +20,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 	{
 		return (NULL);
-	}
-	for (i = 0; i < nmemb; i++)
-	{
-		array[i] = 0;
 	}
 	return (array);
 }
