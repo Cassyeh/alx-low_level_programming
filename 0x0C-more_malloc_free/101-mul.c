e811ec014a5867c991cfb291678bdaab358bc033
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-01-13 06:06:59 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -43,7 +43,7 @@ char *create_xarray(int size)
 	if (array == NULL)
 		exit(98);
 
-	for (index = 0; index < (size - 1); index++)
+	for (index = 0; index < size; index++)
 		array[index] = 'x';
 
 	array[index] = '\0';
