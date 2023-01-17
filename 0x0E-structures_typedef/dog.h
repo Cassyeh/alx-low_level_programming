f4839936f4f7cb990ec4c4b382318a5ef0f3b49e
Cassyeh <cassandraijezie@gmail.com>
2023-01-17 01:50:18 -0800

    Create a new struct Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info for a dog
+ * @name: dog's name in string
+ * @age: dog's age
+ * @owner: dog's owner name in string
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
