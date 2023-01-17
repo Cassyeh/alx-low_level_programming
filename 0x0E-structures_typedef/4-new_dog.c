45dc4b7050ad89464ebba6721b46cfa150756120
Cassyeh <cassandraijezie@gmail.com>
2023-01-17 05:31:16 -0800

    Correction of create a new dog and store copies of elements Task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,12 +26,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*newdog).name = malloc(sizeof(char) * (1 + lenofname));
 	if ((*newdog).name == NULL)
 	{
+		free((*newdog).name);
 		free(newdog);
 		return (NULL);
 	}
 	(*newdog).owner = malloc(sizeof(char) * (1 + lenofowner));
 	if ((*newdog).owner == NULL)
 	{
+		free((*newdog).owner);
 		free(newdog);
 		return (NULL);
 	}
