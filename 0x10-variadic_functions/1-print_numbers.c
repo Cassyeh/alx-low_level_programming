0ada5d0d9a3070e2668085ad1007d6e4dd762d2a
Cassyeh <cassandraijezie@gmail.com>
2023-01-19 18:51:09 -0800

    Print function parameters numbers Task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers with a separator and new line
+ * @separator: the string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	if (n == 0)
+	{
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%d%s", va_arg(ap, int), separator);
+		}
+		else
+		{
+			printf("%d", va_arg(ap, int));
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
