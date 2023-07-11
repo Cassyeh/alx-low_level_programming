ec0648b5e1db153da9857049e55990a9a430dd57
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-07-11 04:23:07 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -35,7 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	count = write(1, buff, i);
-	if (count = -1)
+	if (count == -1)
 	{
 		free(buff);
 		close(filedsc);
